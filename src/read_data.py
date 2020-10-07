7842fe871d0d2026dd99f8a4417f07ce32cd38b1
Simone <simone.barbaro2@studio.unibo.it>
2020-10-07 17:21:05 +0200

    added read data

--- src/read_data.py
+++ src/read_data.py
@@ -1,3 +1,24 @@
 import pandas as pd
 
-data = pd.read_excel("spider_twosides_table.xlsx").set_index(["mol_id", "alldrugs_TWOSIDES"])
+
+def get_spider_data():
+    """
+    Load the spider dataset as it is.
+    :return: A pandas dataframe containing the spider dataset.
+    """
+    data = pd.read_excel("spider_twosides_table.xlsx").set_index(["mol_id", "alldrugs_TWOSIDES"])
+    return data
+
+
+def create_matrix(data):
+    """
+    Create the matrix of the pair interactions from a dataset of interactions.
+    :param data: A dataset of interactions.
+    :return: A new dataframe where each distinct pair of rows is summed together along the columns.
+    """
+    X = []
+    for d1 in data.iterrows():
+        for d2 in data.iterrows():
+            if d1[0] != d2[0]:
+                X.append(d1[1] + d2[1])
+    return pd.DataFrame(X)
